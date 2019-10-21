00fb1bfe2647af04ce41b1ff5d03e4dc1caf9067
ban2002si <banadorah2002@hotmail.com>
2019-10-21 20:29:38 +0300

    Before Publish

---
+++ Bookstore v.1.0/Controllers/BookController.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Models.Repositories;
+using Bookstore.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookstoreRepository<Book> bookrepository;
+        private readonly IBookstoreRepository<Author> authorRepository;
+        private readonly IHostingEnvironment hosting;
+
+        public BookController(IBookstoreRepository<Book> bookrepository,
+            IBookstoreRepository<Author> authorRepository,
+            IHostingEnvironment hosting)
+        {
+            this.bookrepository = bookrepository;
+            this.authorRepository = authorRepository;
+            this.hosting = hosting;
+        }
+        // GET: Book
+        public ActionResult Index()
+        {
+            var books = bookrepository.List();
+            return View(books);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            var book = bookrepository.Find(id);
+            return View(book);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            var model = new BookAuthorViewModel
+            {
+                Authors = FillSelectList()
+            };
+            return View(model);
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookAuthorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string fileName = UploadFile(model.File) ?? string.Empty;
+
+                    if (model.AuthorId == -1)
+                    {
+                        ViewBag.message = "Please Slecet An Author Form The List!!!";
+                        return View(GetAllAuthors());
+                    }
+
+                    var auther = authorRepository.Find(model.AuthorId);
+                    Book book = new Book
+                    {
+                        Id = model.BookId,
+                        Title = model.Title,
+                        Description = model.Description,
+                        Author = auther,
+                        ImageURL = fileName
+                    };
+
+                    bookrepository.Add(book);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            ModelState.AddModelError("", "أدخل المعلومات الإجبارية");
+            return View(GetAllAuthors());
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var book = bookrepository.Find(id);
+            var authorId = book.Author == null ? book.Author.Id = 0 : book.Author.Id; 
+
+            var viewModel = new BookAuthorViewModel
+            {
+                BookId = book.Id,
+                Title = book.Title, 
+                Description = book.Description,
+                AuthorId = authorId,
+                Authors = authorRepository.List().ToList(),
+                ImageURL = book.ImageURL
+            };
+            return View(viewModel);
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BookAuthorViewModel viewModel)
+        {
+            try
+            {
+                string fileName = string.Empty;
+
+                if (viewModel.File != null)
+                {
+                    string uploads = Path.Combine(hosting.WebRootPath, "uploads");
+                    fileName = viewModel.File.FileName;
+                    string fullPath = Path.Combine(uploads, fileName);
+
+                    // Delee Old File
+                    string oldFileName = viewModel.ImageURL;
+                    string oldFullPath = Path.Combine(uploads, oldFileName);
+
+                    // Compare Old With New
+                    if (oldFullPath != fullPath)
+                    {
+                        System.IO.File.Delete(oldFullPath);
+
+                        // Save New File
+                        viewModel.File.CopyTo(new FileStream(fullPath, FileMode.Create));
+                    }
+                }
+
+                var auther = authorRepository.Find(viewModel.AuthorId);
+                Book book = new Book
+                {
+                    Id = viewModel.BookId,
+                    Title = viewModel.Title,
+                    Description = viewModel.Description,
+                    Author = auther,
+                    ImageURL = fileName 
+                };
+
+                bookrepository.Update(viewModel.BookId ,book);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var book = bookrepository.Find(id);
+            return View(book);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                bookrepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        List<Author> FillSelectList()
+        {
+            var authors = authorRepository.List().ToList();
+            authors.Insert(0, new Author { Id = -1, FullName = "--- Select An Author ---" });
+            return authors;
+        }
+
+        BookAuthorViewModel GetAllAuthors()
+        {
+            var vmodel = new BookAuthorViewModel
+            {
+                Authors = FillSelectList()
+            };
+            return vmodel;
+        }
+
+        string UploadFile (IFormFile file)
+        {
+            if (file != null)
+            {
+                string uploads = Path.Combine(hosting.WebRootPath, "uploads");
+                string fullPath = Path.Combine(uploads, file.FileName);
+                file.CopyTo(new FileStream(fullPath, FileMode.Create));
+
+                return file.FileName;
+            }
+
+            return null;
+        }
+
+
+        string UploadFile (IFormFile file, string ImageURL)
+        {
+            if (file != null)
+            {
+                string uploads = Path.Combine(hosting.WebRootPath, "uploads");
+                string newPath = Path.Combine(uploads, file.FileName );
+
+                string oldPath = Path.Combine(uploads,ImageURL);
+
+                // Compare Old With New
+                if (oldPath != newPath)
+                {
+                    System.IO.File.Delete(oldPath);
+
+                    // Save New File
+                    file.CopyTo(new FileStream(newPath, FileMode.Create));
+                }
+
+                return file.FileName;
+            }
+
+            return ImageURL;
+        }
+
+        public ActionResult Search(string term)
+        {
+            var result = bookrepository.Search(term);
+            return View("index",result);
+        }
+    } 
+}
---
+++ Bookstore v.1.0/Models/Repositories/AuthorRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models.Repositories
+{
+    public class AuthorRepository : IBookstoreRepository<Author>
+    {
+        IList<Author> authors;
+
+        public AuthorRepository()
+        {
+            authors = new List<Author>()
+            {
+                new Author {Id=1,FullName="Bandar"},
+                new Author {Id=2,FullName="Abdullah"},
+                new Author {Id=3,FullName="Imam"},
+            };
+        }
+        public void Add(Author entity)
+        {
+            entity.Id = authors.Max(a => a.Id) + 1;
+            authors.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            authors.Remove(author);
+        }
+
+        public Author Find(int id)
+        {
+            var author = authors.SingleOrDefault(a => a.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public List<Author> Search(string term)
+        {
+            return authors.Where(a => a.FullName.Contains(term)).ToList();
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+            var author = Find(id);
+
+            author.FullName = newAuthor.FullName;
+
+        }
+    }
+}
